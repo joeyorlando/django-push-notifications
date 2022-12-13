2ad2cb4a4cf8b75295d83bededd0a7d4809e4ed2
Joey Orlando <joseph.t.orlando@gmail.com>
2022-12-13 09:02:00 +0100

    temporarily remove special logic in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(use_scm_version={"version_scheme": "post-release"})
+# setup(use_scm_version={"version_scheme": "post-release"})
+# TODO: remove this when merging fork to main fork
+setup()
